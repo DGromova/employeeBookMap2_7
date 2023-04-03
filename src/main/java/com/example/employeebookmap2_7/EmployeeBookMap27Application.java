866aa96389b32306667591db76762d9218466f39
Daria Gromova <igromova81@gmail.com>
2023-04-03 19:33:46 +0300

    Start

---
+++ src/main/java/com/example/employeebookmap2_7/EmployeeBookMap27Application.java
@@ -0,0 +1,13 @@
+package com.example.employeebookmap2_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EmployeeBookMap27Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EmployeeBookMap27Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/employeebookmap2_7/EmployeeBookMap27ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.employeebookmap2_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmployeeBookMap27ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
